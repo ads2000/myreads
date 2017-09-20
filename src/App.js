f6e952c8f12283b5fa26dfcb8cdaea50e273a0f2
A2000 <you@example.com>
2017-09-20 14:16:21 +1000

    Stored book data in objects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,58 @@ import React from 'react'
 import './App.css'
 import MoveBooks from './MoveBooks.js'
 
+  const contacts = [
+  {
+    "id": "ryan",
+    "status": "Currently Reading",
+    "title": "To Kill a Mocking Bird",
+    "author": "Harper Lee",
+    "cover": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+  },
+  {
+    "id": "ryan1",
+    "status": "Reading",
+    "title": "Ender's Game",
+    "author": "Orson Scott Card",
+    "cover": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+  },
+    {
+    "id": "ryan",
+    "status": "Currently Reading",
+    "title": "David McCullough",
+    "author": "1776",
+    "cover": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+  },
+  {
+    "id": "ryan1",
+    "status": "Reading",
+    "title": "Harry Potter and the Sorcerer's Stone",
+    "author": "J.K. Rowling",
+    "cover": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+  },
+    {
+    "id": "ryan",
+    "status": "Currently Reading",
+    "title": "The Hobbit",
+    "author": "J.R.R. Tolkien",
+    "cover": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+  },
+  {
+    "id": "ryan1",
+    "status": "Reading",
+    "title": "Oh, the Places You'll Go!",
+    "author": "Seuss",
+    "cover": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+  },
+    {
+    "id": "ryan1",
+    "status": "Reading",
+    "title": "The Adventures of Tom Sawyer",
+    "author": "Mark Twain",
+    "cover": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+  }
+]
+
 class BooksApp extends React.Component {
   state = {
     /**
@@ -13,6 +65,7 @@ class BooksApp extends React.Component {
      */
     showSearchPage: false
   }
+
 
   render() {
     return (
@@ -39,7 +92,18 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : ( 
-           <MoveBooks />
+           <div>
+                     <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+           <MoveBooks contacts={contacts} bookStatus="Currently Reading"/>
+           <MoveBooks contacts={contacts} bookStatus="Want to Read"/>
+           <MoveBooks contacts={contacts} bookStatus="Read"/>
+           </div></div></div>
+           </div>
           
         )}
       </div>
